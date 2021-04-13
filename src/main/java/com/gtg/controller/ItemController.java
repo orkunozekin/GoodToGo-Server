9ebe44765cc934e2271824b879aa59bab43cac4d
Orkun Ozekin <orkunozekin@gmail.com>
2021-04-13 09:32:34 -0500

    deploying

--- src/main/java/com/gtg/controller/ItemController.java
+++ src/main/java/com/gtg/controller/ItemController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gtg.model.Item;
@@ -40,8 +39,6 @@ public class ItemController {
 		return "Failed to create item. Try again!"; 
 	}
 	
-	@GetMapping()
-
 	
 	@Autowired
 	public ItemController(ItemRepository itemRepo) {
